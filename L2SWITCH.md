aae5016ebf180ea22223edbce42170fac306d294
Arie Oldman <arie@spriggy.com.au>
2019-05-22 21:53:33 +1000

    Working L2 switch, with demonstration http filtering.

--- L2SWITCH.md
+++ L2SWITCH.md
@@ -20,6 +20,10 @@
 
 When a switch connects to a controller, the switch immediately sends an OpenFlow Hello message. The controller will response with a Hello Response. This is used to negotiate OpenFlow version support.
 
+We need to implement this handshake:
+
+![handshake](http://sdnhub.org/wp-content/uploads/2014/02/OF_Msg_Exchanges.png)
+
 After the Hello Response is sent, our controller sends two flows:
 
 1. A flow entry on table 0, which matches all packets, with priority 100, with these instructions:
@@ -67,136 +71,3 @@ Let's add one more flow entry, on the same PacketIn event.
    1. Go to table 1
 
 Notice the **priority**? Matches are sorted by descending priority. If multiple matches are possible, the switch chooses the one with highest priority. This ensures that we will skip the flow entry that forwards packets to the switch, if the node it comes from has already been learned.
-
-## Go Implementation
-
-Note that this is an example, and won't compile on its own.
-
-### Initialisation (Hello)
-
-```go
-helloEvent := of.TypeMatcher(of.TypeHello)
-
-mux := of.NewServeMux()
-
-gotoForwardingTable := &ofp.InstructionGotoTable{
-fwdTable,
-}
-```
-
-```go
-mux.HandleFunc(helloEvent, func(rw of.ResponseWriter, r *of.Request){
-
-    //Send back the Hello response
-    glog.Infoln("Responded to", of.TypeHello, "from host", r.Addr, ".")
-
-    rw.Write(&of.Header{Type: of.TypeHello}, nil)
-
-    controller := &ofp.InstructionApplyActions{
-        ofp.Actions{
-            &ofp.ActionOutput{ofp.PortController, ofp.ContentLenMax},
-        },
-    }
-
-    flood := &ofp.InstructionApplyActions{
-        ofp.Actions{
-            &ofp.ActionOutput{ofp.PortFlood, 0},
-        },
-    }
-
-    matchEverything := ofp.XM{
-        Class:  ofp.XMClassOpenflowBasic,
-        Type:   ofp.XMTypeEthDst,
-        Value:  ofp.XMValue{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-        // A mask of 6 0x00 bytes tells the switch that we don't actually care about any
-        // bits in the ethernet address - this will always match.
-        Mask:   ofp.XMValue{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-    }
-
-    // Packets arriving at the ctrlTable will be sent to the controller and then
-    // the fwdTable.
-    flowModCtrl := ofp.NewFlowMod(ofp.FlowAdd, nil)
-    flowModCtrl.Match = ofputil.ExtendedMatch(matchEverything)
-    flowModCtrl.Instructions = ofp.Instructions{controller, gotoForwardingTable,}
-    flowModCtrl.Priority = 100
-    flowModCtrl.Table = ctrlTable
-
-    rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModCtrl)
-
-    // All packets which can't be matched, explicitly flood to all remaining ports.
-    flowModCustomMiss := ofp.NewFlowMod(ofp.FlowAdd, nil)
-    flowModCustomMiss.Match = ofputil.ExtendedMatch(matchEverything)
-    flowModCustomMiss.Instructions = ofp.Instructions{flood}
-    flowModCustomMiss.Priority = 100
-    flowModCustomMiss.Table = fwdTable
-
-    rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModCustomMiss)
-
-})
-```
-
-### PacketIn
-
-```go
-packetInEvent := of.TypeMatcher(of.TypePacketIn)
-```
-
-```go
-mux.HandleFunc(packetInEvent, func( rw of.ResponseWriter, r *of.Request){
-
-    glog.Infoln("PacketIn Message from host", r.Addr)
-
-    var packet ofp.PacketIn
-    packet.ReadFrom(r.Body)
-
-    var ingressPort ofp.XMValue
-
-    ingressPort = packet.Match.Field(ofp.XMTypeInPort).Value
-
-    // Instruction to immediately output the packet on the ingress port
-    portOutput := &ofp.InstructionApplyActions{
-        ofp.Actions{
-            &ofp.ActionOutput{ofp.PortNo(ingressPort.UInt32()), 0},
-        },
-    }
-
-    var packetDecode layers.Ethernet
-    packetDecode.DecodeFromBytes(packet.Data, gopacket.NilDecodeFeedback)
-
-    glog.Infof("Src MAC: %x, Dst MAC: %x", []byte(packetDecode.SrcMAC), []byte(packetDecode.DstMAC))
-
-    matchEthDst := ofp.XM{
-        Class: ofp.XMClassOpenflowBasic,
-        Type:  ofp.XMTypeEthDst,
-        Value: ofp.XMValue(packetDecode.SrcMAC),
-    }
-
-    matchEthSrc := ofp.XM{
-        Class: ofp.XMClassOpenflowBasic,
-        Type:  ofp.XMTypeEthSrc,
-        Value: ofp.XMValue(packetDecode.SrcMAC),
-    }
-
-    // Add a flow to the fwdTable which matches the packet destination to an output port.
-    flowModLearn := ofp.NewFlowMod(ofp.FlowAdd, nil)
-    flowModLearn.Match = ofputil.ExtendedMatch(matchEthDst)
-    flowModLearn.Instructions = ofp.Instructions{portOutput}
-    flowModLearn.IdleTimeout = 300
-    flowModLearn.Priority = 200
-    flowModLearn.Table = fwdTable
-
-    rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModLearn)
-
-    // Add a flow to the ctrlTable which matches the packet source in order to avoid
-    // sending the packet to the controller if the mapping has already been learned.
-    flowModSkipPacketIn := ofp.NewFlowMod(ofp.FlowAdd, nil)
-    flowModSkipPacketIn.Match = ofputil.ExtendedMatch(matchEthSrc)
-    flowModSkipPacketIn.Instructions = ofp.Instructions{gotoForwardingTable}
-    flowModSkipPacketIn.IdleTimeout = 300
-    flowModSkipPacketIn.Priority = 200
-    flowModSkipPacketIn.Table = ctrlTable
-
-    rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModSkipPacketIn)
-
- })
-```
--- main.go
+++ main.go
@@ -24,6 +24,8 @@ func main() {
 	helloEvent := of.TypeMatcher(of.TypeHello)
 	packetInEvent := of.TypeMatcher(of.TypePacketIn)
 	errorEvent := of.TypeMatcher(of.TypeError)
+	echoRequestEvent := of.TypeMatcher(of.TypeEchoRequest)
+	featuresReplyEvent := of.TypeMatcher(of.TypeFeaturesReply)
 
 	mux := of.NewServeMux()
 
@@ -38,24 +40,29 @@ func main() {
 		fwdTable,
 	}
 
-	mux.HandleFunc(helloEvent, func(rw of.ResponseWriter, r *of.Request){
-		//Send back the Hello response
+	controller := &ofp.InstructionApplyActions{
+		ofp.Actions{
+			&ofp.ActionOutput{ofp.PortController, ofp.ContentLenMax},
+		},
+	}
 
-		glog.Infoln("Responded to", of.TypeHello, "from host", r.Addr, ".")
+	flood := &ofp.InstructionApplyActions{
+		ofp.Actions{
+			&ofp.ActionOutput{ofp.PortFlood, 0},
+		},
+	}
 
-		rw.Write(&of.Header{Type: of.TypeHello}, nil)
+	dropPacket := &ofp.InstructionApplyActions{
+		ofp.Actions{
+			//Empty actions means we drop the packet.
+		},
+	}
 
-		controller := &ofp.InstructionApplyActions{
-			ofp.Actions{
-				&ofp.ActionOutput{ofp.PortController, ofp.ContentLenMax},
-			},
-		}
+	mux.HandleFunc(featuresReplyEvent, func(rw of.ResponseWriter, r * of.Request){
+		var featuresReply ofp.SwitchFeatures
+		featuresReply.ReadFrom(r.Body)
 
-		flood := &ofp.InstructionApplyActions{
-			ofp.Actions{
-				&ofp.ActionOutput{ofp.PortFlood, 0},
-			},
-		}
+		glog.Infof("Features Reply from %s: DatapathID %x, %v\n", r.Addr, featuresReply.DatapathID, featuresReply)
 
 		matchEverything := ofp.XM{
 			Class:	ofp.XMClassOpenflowBasic,
@@ -65,25 +72,84 @@ func main() {
 		}
 
 		// Packets arriving at the ctrlTable will be sent to the controller and then the fwdTable.
-		FlowModCtrl := ofp.NewFlowMod(ofp.FlowAdd, nil)
-		FlowModCtrl.Match = ofputil.ExtendedMatch(matchEverything)
-		FlowModCtrl.Instructions = ofp.Instructions{controller, gotoForwardingTable,}
-		FlowModCtrl.HardTimeout = 0
-		FlowModCtrl.Priority = 100
-		FlowModCtrl.Table = ctrlTable
+		flowModCtrl := ofp.NewFlowMod(ofp.FlowAdd, nil)
+		flowModCtrl.Match = ofputil.ExtendedMatch(matchEverything)
+		flowModCtrl.Instructions = ofp.Instructions{controller, gotoForwardingTable,}
+		flowModCtrl.HardTimeout = 0
+		flowModCtrl.Priority = 100
+		flowModCtrl.Table = ctrlTable
 
-		rw.Write(&of.Header{Type: of.TypeFlowMod}, FlowModCtrl)
+		rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModCtrl)
 
 		// All packets which can't be matched, explicitly flood to all remaining ports.
-		FlowModCustomMiss := ofp.NewFlowMod(ofp.FlowAdd, nil)
-		FlowModCustomMiss.Match = ofputil.ExtendedMatch(matchEverything)
-		FlowModCustomMiss.Instructions = ofp.Instructions{flood}
-		FlowModCustomMiss.HardTimeout = 0
-		FlowModCustomMiss.Priority = 100
-		FlowModCustomMiss.Table = fwdTable
+		flowModCustomMiss := ofp.NewFlowMod(ofp.FlowAdd, nil)
+		flowModCustomMiss.Match = ofputil.ExtendedMatch(matchEverything)
+		flowModCustomMiss.Instructions = ofp.Instructions{flood}
+		flowModCustomMiss.HardTimeout = 0
+		flowModCustomMiss.Priority = 100
+		flowModCustomMiss.Table = fwdTable
 
-		rw.Write(&of.Header{Type: of.TypeFlowMod}, FlowModCustomMiss)
+		rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModCustomMiss)
 
+		// Example: block all tcp packets via port 80 using a flow mod
+		// 	Prerequisites: 
+		//		OXM_OF_ETH_TYPE in (0x0800, 0x86dd)
+		//		OXM_OF_IP_PROTO in (0x06)
+
+		matchEthType0800 := ofp.XM{
+			Class:	ofp.XMClassOpenflowBasic,
+			Type:	ofp.XMTypeEthType,
+			Value:	ofp.XMValue{0x08, 0x00,},
+		}
+
+		matchIPProto6 := ofp.XM{
+			Class:	ofp.XMClassOpenflowBasic,
+			Type:	ofp.XMTypeIPProto,
+			Value:	ofp.XMValue{0x06,},
+		}
+		matchHTTP := ofp.XM{
+			Class:	ofp.XMClassOpenflowBasic,
+			Type:	ofp.XMTypeTCPDst,
+			Value:	ofp.XMValue{0x00, 0x50,}, // 80 in base 16
+		}
+
+		flowModBlockHTTP := ofp.NewFlowMod(ofp.FlowAdd, nil)
+		flowModBlockHTTP.Match = ofputil.ExtendedMatch(
+			matchEthType0800,
+			matchIPProto6,
+			matchHTTP,
+		)
+		flowModBlockHTTP.Instructions = ofp.Instructions{dropPacket}
+		flowModBlockHTTP.Priority = 1000
+		flowModBlockHTTP.Table = ctrlTable
+
+		rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModBlockHTTP)
+	})
+
+	mux.HandleFunc(helloEvent, func(rw of.ResponseWriter, r *of.Request){
+		//Send back the Hello response
+
+		glog.Infoln("Responded to", of.TypeHello, "from host", r.Addr, ".")
+
+		rw.Write(&of.Header{Type: of.TypeHello}, nil)
+
+		// Features Request
+		glog.Infoln("Features Request to ", r.Addr)
+		rw.Write(&of.Header{Type: of.TypeFeaturesRequest}, nil)
+
+	})
+	
+	mux.HandleFunc(echoRequestEvent, func( rw of.ResponseWriter, r *of.Request){
+		glog.Infoln("Echo request from",r.Addr,". Replying.")
+
+		var req ofp.EchoRequest
+		req.ReadFrom(r.Body)
+
+		echoReply := of.NewRequest(of.TypeEchoReply, &ofp.EchoReply{
+			Data: req.Data,
+		})
+
+		rw.Write(&of.Header{Type: of.TypeEchoReply}, echoReply)
 	})
 
 	mux.HandleFunc(packetInEvent, func( rw of.ResponseWriter, r *of.Request){
@@ -108,7 +174,7 @@ func main() {
 		packetDecode.DecodeFromBytes(packet.Data, gopacket.NilDecodeFeedback)
 
 		glog.Infof("Src MAC: %x, Dst MAC: %x", []byte(packetDecode.SrcMAC), []byte(packetDecode.DstMAC))
-
+		
 		matchEthDst := ofp.XM{
 			Class:	ofp.XMClassOpenflowBasic,
 			Type:	ofp.XMTypeEthDst,
@@ -122,25 +188,25 @@ func main() {
 		}
 
 		// Add a flow to the fwdTable which matches the packet destination to an output port.
-		FlowModLearn := ofp.NewFlowMod(ofp.FlowAdd, nil)
-		FlowModLearn.Match = ofputil.ExtendedMatch(matchEthDst)
-		FlowModLearn.Instructions = ofp.Instructions{portOutput}
-		FlowModLearn.IdleTimeout = 300
-		FlowModLearn.Priority = 200
-		FlowModLearn.Table = fwdTable
+		flowModLearn := ofp.NewFlowMod(ofp.FlowAdd, nil)
+		flowModLearn.Match = ofputil.ExtendedMatch(matchEthDst)
+		flowModLearn.Instructions = ofp.Instructions{portOutput}
+		flowModLearn.IdleTimeout = 300
+		flowModLearn.Priority = 200
+		flowModLearn.Table = fwdTable
 
-		rw.Write(&of.Header{Type: of.TypeFlowMod}, FlowModLearn)
+		rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModLearn)
 
 		// Add a flow to the ctrlTable which matches the packet source in order to avoid 
 		// sending the packet to the controller if the mapping has already been learned.
-		FlowModSkipPacketIn := ofp.NewFlowMod(ofp.FlowAdd, nil)
-		FlowModSkipPacketIn.Match = ofputil.ExtendedMatch(matchEthSrc)
-		FlowModSkipPacketIn.Instructions = ofp.Instructions{gotoForwardingTable}
-		FlowModSkipPacketIn.IdleTimeout = 300
-		FlowModSkipPacketIn.Priority = 200
-		FlowModSkipPacketIn.Table = ctrlTable
+		flowModSkipPacketIn := ofp.NewFlowMod(ofp.FlowAdd, nil)
+		flowModSkipPacketIn.Match = ofputil.ExtendedMatch(matchEthSrc)
+		flowModSkipPacketIn.Instructions = ofp.Instructions{gotoForwardingTable}
+		flowModSkipPacketIn.IdleTimeout = 300
+		flowModSkipPacketIn.Priority = 200
+		flowModSkipPacketIn.Table = ctrlTable
 
-		rw.Write(&of.Header{Type: of.TypeFlowMod}, FlowModSkipPacketIn)
+		rw.Write(&of.Header{Type: of.TypeFlowMod}, flowModSkipPacketIn)
 
 	})
 
